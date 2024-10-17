feee0c1fbd140823f3ef1cd208ae244b44050890
Cloudy <87387633+cloudyio@users.noreply.github.com>
2024-10-17 21:03:26 +0100

    made Authentication

--- app/(dash)/login/page.tsx
+++ app/(dash)/login/page.tsx
@@ -1,16 +1,46 @@
+'use client';
+
 import {Input} from "@nextui-org/input";
+import { useState } from 'react';
+import { signUp, logIn } from '../../../libs/auth';
+
 
 export default function HomePage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isLogin, setIsLogin] = useState(true);
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        try {
+          if (isLogin) {
+            await logIn(email, password);
+            alert('Login successful!');
+          } else {
+            await signUp(email, password);
+            alert('Sign up successful!');
+          }
+        } catch (error: any) {
+          alert(error.message);
+        }
+      };
+
+
     return (
         <div className="wrapper flex justify-center content-center items-center		">
-        <form>
+        <form onSubmit={handleSubmit}>
             <h1>Login</h1>
             <div className="input-box">   
-                <Input type="email" label="Email" />
+                <Input type="email" label="Email" value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required />
+
             <i className='bx bxs-user'></i>
             </div>
             <div className="input-box">   
-            <Input type="email" label="Email" />
+            <Input type="password" label="Password"  value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required/>
             <i className='bx bxs-lock'></i>
             </div>
 
@@ -20,8 +50,8 @@ export default function HomePage() {
             
             </div>
 
-            <button type="submit" className="btn">Login</button>
-
+            <button type="submit" className="btn" onClick={() => setIsLogin(isLogin)}>Login</button>
+            <button type="submit" className="btn" onClick={() => setIsLogin(!isLogin)}>Sign Up</button>
             <div className="register-link">
                 <p>Dont have an account? <a href="#">Register Here</a></p>
             </div>
--- libs/firebaseConfig.ts
+++ libs/firebaseConfig.ts
@@ -4,7 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_APIKEY,
+  apiKey: 'AIzaSyBOU1NfBDOPxHAqkJTKoX4qOGV2zLqXAm8',
   authDomain: process.env.FIREBASE_AUTHDOMAIN,
   projectId: process.env.FIREBASE_PROJECTID,
   storageBucket: process.env.FIREBASE_STORAGEBUCKET,
