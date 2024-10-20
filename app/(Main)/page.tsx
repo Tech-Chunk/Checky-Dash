2df8ed39bd239dda5e82e884d4b06628190bdd86
Domo <179413444+DomoSouthwell@users.noreply.github.com>
2024-10-20 18:12:10 +0100

    egg

--- app/(Main)/page.tsx
+++ app/(Main)/page.tsx
@@ -1,16 +1,16 @@
 import  "./home.css";
-
+import {Button,ButtonGroup} from "@nextui-org/button"
 
 export default function HomePage() {
     return (
   <div className="banner w-full">
     <div className="content">
-      <h1>Checky - The all in one check in app</h1>
+      <h1 className="font-bold">The all in one check in app</h1>
       <p>The Checky app can be downloaded and then all information can be accessed using the dashboard here</p>
-      <div>
-        <button className="button" type="button"><span></span>Download</button>
-        <button className="button" type="button"><span></span>Learn More</button>
-      </div>
+      <ButtonGroup>
+        <Button className="button" type="button" size="lg"><span></span>Download</Button>
+        <Button className="button" type="button" size="lg"><span></span>Learn More</Button>
+      </ButtonGroup>
     </div>
   </div>
   
