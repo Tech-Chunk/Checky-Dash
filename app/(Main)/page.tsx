50613a32218c5e6e667e7fa8963bb07fa30b72c2
Domo <179413444+DomoSouthwell@users.noreply.github.com>
2024-10-20 18:58:44 +0100

    dashy

--- app/(Main)/page.tsx
+++ app/(Main)/page.tsx
@@ -5,14 +5,22 @@ export default function HomePage() {
     return (
   <div className="banner w-full">
     <div className="content">
-      <h1 className="font-bold">The all in one check in app</h1>
-      <p>The Checky app can be downloaded and then all information can be accessed using the dashboard here</p>
-      <ButtonGroup>
-        <Button className="button" type="button" size="lg"><span></span>Download</Button>
-        <Button className="button" type="button" size="lg"><span></span>Learn More</Button>
-      </ButtonGroup>
+      <h1 className="font-bold">Live tracking of staff's entrys, comfort knowing who is on and off the property and another level of saftey. Only with Checky.</h1>
+      <p>The Checky app can be downloaded on Android and IOS devices, and then all entry and exit information can be accessed using the dashboard here</p>
+      <div className="flex flex-wrap gap-4 items-center">
+      <Button color="primary" variant="solid">
+        Download
+      </Button>
+      <Button color="primary" variant="faded">
+        Learn More
+      </Button>  
+      
+    </div>
     </div>
   </div>
   
+  
     );
 }
+
+
