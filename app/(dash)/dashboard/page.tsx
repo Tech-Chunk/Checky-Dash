a94701f0b7c05348a9623350336c3935bd0a4fdd
ChomBot <charlie.fox2030@gmail.com>
2024-11-09 21:49:31 +0000

    Skelteton

--- app/(dash)/dashboard/page.tsx
+++ app/(dash)/dashboard/page.tsx
@@ -101,6 +101,32 @@ export default function Home() {
             </div> 
               </CardBody>
             </Card>
+            <Card className="w-[95%]" isPressable>
+              <CardBody className="flex flex-row gap-4 p-4 content-center items-center">
+              <div className="max-w-[300px] w-full flex items-center gap-3">
+             <div>
+                <Skeleton className="flex rounded-full w-12 h-12"/>
+              </div>  
+              <div className="w-full flex flex-col gap-2">
+                <Skeleton className="h-3 w-3/5 rounded-lg"/>
+                <Skeleton className="h-3 w-4/5 rounded-lg"/>
+              </div>
+            </div> 
+              </CardBody>
+            </Card>
+            <Card className="w-[95%]" isPressable>
+              <CardBody className="flex flex-row gap-4 p-4 content-center items-center">
+              <div className="max-w-[300px] w-full flex items-center gap-3">
+             <div>
+                <Skeleton className="flex rounded-full w-12 h-12"/>
+              </div>  
+              <div className="w-full flex flex-col gap-2">
+                <Skeleton className="h-3 w-3/5 rounded-lg"/>
+                <Skeleton className="h-3 w-4/5 rounded-lg"/>
+              </div>
+            </div> 
+              </CardBody>
+            </Card>
                 
             
           </CardHeader>
